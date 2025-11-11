862c603d2c6e4fe71e1ca40992b57e0605318000
carriel <carrielgbr@gmail.com>
2025-11-10 21:45:29 -0300

    correção temporária

--- int.py
+++ int.py
@@ -2,26 +2,15 @@
 
 
 #nome = input("Qual é o seu nome ? ")
-salario = input("Qual é o seu salario ?")
-
-salarioFormatado = "{:,.2f}".format(salario)
-# Substitui o ponto decimal por vírgula para exibição brasileiras
-salarioFormatadoVirgula = salarioFormatado.replace('.', ',')
-
-# print(texto_formatado_com_virgula)
-# Saída: 1.234,57
-
-
+salario = float(input("Qual é o seu salario ?"))
 horasTrabalhadas = float(input("Quantas horas trabalhas na semana ?"))
 
 #print(f"Seu nome é: {nome}")
 ##print(f"Seu salário:{3000}")
 
-valorDia = salarioFormatadoVirgula/horasTrabalhadas
+valorDia = salario/horasTrabalhadas
 
-#round (valorDia, 2)
-
-print(f"Sua diaria vale:")
+print(f"Sua diaria vale: {valorDia: .2f}")
 print ("-------------------------------")
 horasDia = float(input("quantas horas voce trabalha por dia ?"))
 print (f'Sua hora é: {valorDia / horasDia: .2f}')
