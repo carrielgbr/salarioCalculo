e0660fc2a614d4c5dde321ba553a19e875473c3a
carriel <carrielgbr@gmail.com>
2025-11-10 21:41:37 -0300

    erro na conversão adição de virgula no projeto no momento só trabalhamos com pontos

--- int.py
+++ int.py
@@ -1,24 +1,27 @@
 ## Calculo de salário 
 
-import locale
-
-locale.setlocale(locale.LC_ALL, 'Portuguese_Brazil.1252')
 
 #nome = input("Qual é o seu nome ? ")
-salario = float(input("Qual é o seu salario ?"))
+salario = input("Qual é o seu salario ?")
 
-new_salario = locale.atof(salario)
+salarioFormatado = "{:,.2f}".format(salario)
+# Substitui o ponto decimal por vírgula para exibição brasileiras
+salarioFormatadoVirgula = salarioFormatado.replace('.', ',')
+
+# print(texto_formatado_com_virgula)
+# Saída: 1.234,57
+
 
 horasTrabalhadas = float(input("Quantas horas trabalhas na semana ?"))
 
 #print(f"Seu nome é: {nome}")
 ##print(f"Seu salário:{3000}")
 
-valorDia = salario/horasTrabalhadas
+valorDia = salarioFormatadoVirgula/horasTrabalhadas
 
 #round (valorDia, 2)
 
-print(f"Sua diaria vale: {valorDia: .2f}")
+print(f"Sua diaria vale:")
 print ("-------------------------------")
 horasDia = float(input("quantas horas voce trabalha por dia ?"))
 print (f'Sua hora é: {valorDia / horasDia: .2f}')
